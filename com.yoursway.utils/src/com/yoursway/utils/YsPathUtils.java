980fd9af56511c2a45e4a8dc40009b2f040cc1c3
Andrey Tarantsov <andreyvit@gmail.com>
2008-07-16 20:06:45 +0700

    Added to YsPathUtils: removeTrailingSeparator, isPathSeparator, isChildOrParent.

--- com.yoursway.utils/src/com/yoursway/utils/YsPathUtils.java
+++ com.yoursway.utils/src/com/yoursway/utils/YsPathUtils.java
@@ -1,5 +1,7 @@
 package com.yoursway.utils;
 
+import static com.yoursway.utils.YsPathUtils.isPathSeparator;
+
 import java.io.File;
 
 public class YsPathUtils {
@@ -15,7 +17,7 @@ public class YsPathUtils {
         else
             return null;
     }
-
+    
     public static String joinPath(String a, String b) {
         if (a.length() == 0)
             return b;
@@ -29,6 +31,33 @@ public class YsPathUtils {
             return a + "/" + b;
         else
             return a + b;
+    }
+    
+    public static String removeTrailingSeparator(String directoryPath) {
+        if (directoryPath.endsWith("/") || directoryPath.endsWith(File.pathSeparator))
+            return directoryPath.substring(0, directoryPath.length() - 1);
+        else
+            return directoryPath;
+    }
+    
+    public static boolean isPathSeparator(char ch) {
+        return ch == '/' || ch == File.pathSeparatorChar;
+    }
+    
+    public static boolean isChildOrParent(String first, String second) {
+        int firstLength = first.length();
+        int secondLength = second.length();
+        if (secondLength > firstLength)
+            // might be a child
+            return second.regionMatches(0, first, 0, firstLength)
+                    && isPathSeparator(second.charAt(firstLength));
+        else if (secondLength < firstLength)
+            // might be a parent
+            return first.regionMatches(0, second, 0, secondLength)
+                    && isPathSeparator(first.charAt(secondLength));
+        else
+            // pathLength == monitoredPathLength
+            return second.equals(first);
     }
     
 }
