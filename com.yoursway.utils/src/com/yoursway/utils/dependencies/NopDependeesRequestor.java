441bdb8cd6b936e0b2d9f812b20a337813d835a0
Andrey Tarantsov <andreyvit@gmail.com>
2008-07-31 19:58:56 +0700

    Dependency Tracker: added an ability to listen to the "section exit" event.

---
+++ com.yoursway.utils/src/com/yoursway/utils/dependencies/NopDependeesRequestor.java
@@ -0,0 +1,9 @@
+package com.yoursway.utils.dependencies;
+
+public class NopDependeesRequestor implements DependeesRequestor {
+
+    public void dependsOn(Dependee dependee) {
+        // nop
+    }
+    
+}
