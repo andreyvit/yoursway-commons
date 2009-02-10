4636f4d9f45dcc8579ee28a809d43d0dbfb25fe2
Andrey Tarantsov <andreyvit@gmail.com>
2009-02-10 19:23:12 +0600

    @NonTestableThinClass annotation.

---
+++ com.yoursway.utils/src/com/yoursway/utils/annotations/NonTestableThinClass.java
@@ -0,0 +1,10 @@
+package com.yoursway.utils.annotations;
+
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Target;
+
+@Target(TYPE)
+public @interface NonTestableThinClass {
+
+}
